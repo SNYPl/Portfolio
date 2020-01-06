3f81c582a8852b047e69b2021f62a5017de10716
Daviti Metreveli <SNYPl@users.noreply.github.com>
2020-01-06 22:54:36 +0400

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,11 @@ let closebtn3 = document.getElementById('btn3');
 let navslide = document.getElementById('nav-background');
 
 let vnaxot= function () {
-   setInterval(function () {
+
     for (i = 1; i <= 45; i++) {
 
         navslide.style.width = i++ + "%";
     }
-}, 500);
 };
 
 wrap.addEventListener('click', function() {
@@ -20,6 +19,7 @@ wrap.addEventListener('click', function() {
     closebtn2.classList.toggle('bt2');
     closebtn3.classList.toggle('bt3');
     navslide.classList.toggle('slidebackground');
+
 vnaxot();
-    // setInterval(vnaxot, 500);
+
 });
