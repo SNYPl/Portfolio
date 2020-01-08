53ff271219ab4568ba66b985149d16652e398aaa
Daviti Metreveli <SNYPl@users.noreply.github.com>
2020-01-09 00:13:55 +0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let closebtn1 = document.getElementById('btn1');
 let closebtn2 = document.getElementById('btn2');
 let closebtn3 = document.getElementById('btn3');
 let navslide = document.getElementById('nav-background');
+let fnd = document.querySelector('slidebackground');
 
 let vnaxot= function () {
 
@@ -13,6 +14,20 @@ let vnaxot= function () {
     }
 };
 
+
+
+let reverse = function () {
+    if (navslide.className==="slidebackground") {
+        navslide.style.animationDirection ="normal";
+
+    }else {
+        navslide.classList.toggle('slidebackground-close');
+    }
+
+};
+
+
+
 wrap.addEventListener('click', function() {
     test.classList.toggle('hidden');
     closebtn1.classList.toggle('bt1');
@@ -20,6 +35,8 @@ wrap.addEventListener('click', function() {
     closebtn3.classList.toggle('bt3');
     navslide.classList.toggle('slidebackground');
 
-vnaxot();
+
+
+reverse();
 
 });
