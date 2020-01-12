5bf9b37889f0d924cda62541acc5f406b67110ee
Daviti Metreveli <SNYPl@users.noreply.github.com>
2020-01-13 02:59:53 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,20 @@
 let wrap= document.getElementById('wrapp');
-let test = document.getElementById('toggle');
+let toggle = document.getElementById('toggle');
 let closebtn1 = document.getElementById('btn1');
 let closebtn2 = document.getElementById('btn2');
 let closebtn3 = document.getElementById('btn3');
 let navslide = document.getElementById('nav-background');
-let fnd = document.querySelector('slidebackground');
-
-let vnaxot= function () {
-
-    for (i = 1; i <= 45; i++) {
-
-        navslide.style.width = i++ + "%";
-    }
-};
+let scslide = document.getElementById("secondSlide");
 
 
 
 let reverse = function () {
-    if (navslide.className==="slidebackground") {
+    if (navslide.className==="slidebackground" && scslide.className==="slidebackground-up") {
         navslide.style.animationDirection ="normal";
 
     }else {
         navslide.classList.toggle('slidebackground-close');
+        scslide.classList.toggle('slidebackgroundup-close');
     }
 
 };
@@ -29,11 +22,12 @@ let reverse = function () {
 
 
 wrap.addEventListener('click', function() {
-    test.classList.toggle('hidden');
+    toggle.classList.toggle('hidden');
     closebtn1.classList.toggle('bt1');
     closebtn2.classList.toggle('bt2');
     closebtn3.classList.toggle('bt3');
     navslide.classList.toggle('slidebackground');
+    scslide.classList.toggle('slidebackground-up');
 
 
 
